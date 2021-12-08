0edec3030832557f1811081119a3c6e30b2d2274
Reza <rezafisher3@gmail.com>
2021-12-08 13:30:33 -0600

    add testimonial

---
+++ index.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+const swiper2 = new Swiper('.swiper2', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
